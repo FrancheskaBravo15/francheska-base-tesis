640853436d1b42a9ffbc322b4fbbdafbea996e35
Darwinpz <dpilaloazea@gmail.com>
2026-04-18 12:34:04 -0500

    fix desing
    Arreglando diseño de los elementos en el index

--- controllers/indexController.py
+++ controllers/indexController.py
@@ -2,17 +2,20 @@ from flask import render_template, Blueprint, request, flash, redirect, url_for
 from services.serviceService import ServiceService
 from services.categoryService import CategoryService
 from services.testimonialService import TestimonialService
+from services.promotionService import PromotionService
 
 index_bp = Blueprint("index", __name__, url_prefix='/')
 
 @index_bp.route('/', methods=['GET'])
 def indexRoute():
-    result    = ServiceService.get_all_services(only_active=True)
-    services  = result.get("services", [])[:6]
-    categories = CategoryService.get_all_categories().get("categories", [])
+    result       = ServiceService.get_all_services(only_active=True)
+    services     = result.get("services", [])[:6]
+    categories   = CategoryService.get_all_categories().get("categories", [])
     testimonials = TestimonialService.get_approved(limit=3).get("testimonials", [])
+    promotions   = PromotionService.get_all_promotions(only_active=True).get("promotions", [])[:3]
     return render_template('/views/index.html', services=services,
-                           categories=categories, testimonials=testimonials)
+                           categories=categories, testimonials=testimonials,
+                           promotions=promotions)
 
 @index_bp.route('/about', methods=['GET'])
 def aboutRoute():
